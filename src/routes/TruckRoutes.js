54edc84424946f02a14656fd145471b1ae3154b6
Jordan Ohlwiler <ohlwilerj@gmail.com>
2019-10-28 17:06:14 -0600

    route paths set up

---
+++ src/routes/TruckRoutes.js
@@ -0,0 +1,15 @@
+import {Switch, Route} from 'react-router-dom'
+import TruckDash from '../component/trucks/TruckDash/TruckDash'
+import AddEvent from '../component/trucks/AddEvent/AddEvent'
+import JoinEvent from '../component/trucks/JoinEvent/JoinEvent'
+import MyMenu from '../component/trucks/MyMenu/MyMenu'
+
+
+export default ( 
+    <Switch>
+        <Route exact path='/' component={TruckDash} />
+        <Route path ='/addevent' component={AddEvent} />
+        <Route path ='/joinevent' component={JoinEvent} />
+        <Route path ='/mymenu' component={MyMenu} />
+    </Switch>
+)
