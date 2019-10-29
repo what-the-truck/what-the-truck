a994e3a13747c97976c8902658dda94529c5d820
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-10-29 11:00:14 -0600

    login

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
   return (
     <div className="App">
     <Header />  
-    {TruckRoutes}
-    {CustRoutes}
+   
 
     </div>
   );
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,11 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { setUser } from "../ducks/userReducer";
+import { withRouter, Link } from "react-router-dom";
+import swal from "sweetalert2";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+export class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = (e, key) => {
+    this.setState({
+      [key]: e.target.value
+    });
+  };
+
+  // This is user login
+  userlogin = async () => {
+    const { email, password } = this.state;
+    const res = await axios.post("/auth/userlogin", { email, password });
+    if (res.data.user) {
+      this.props.setUser(res.data.user);
     }
+    //   console.log(res.data.user)
+    swal.fire({ type: "success", text: res.data.message });
+    this.props.history("/");
+  };
+
+  // This is truck login
+  trucklogin = async () => {
+    const { email, password } = this.state;
+    axios.post("/auth/trucklogin", { email, password });
+    if (res.data.user) {
+      this.props.setUser(res.data.user);
+    }
+    // console.log(res.data.user)
+    swal.fire({ type: "success", text: res.data.message });
+    this.props.history("/");
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={e => this.handleChange(e, "email")}
+        />
+        <input
+        type="password"
+        placeholder="Password"
+        onChange={e => this.handleChange(e, "password")}
+      />
+      <button onClick={this.userlogin}>User Login</button>
+      <button onClick={this.trucklogin}>truck Login</button>
+      </div>
+    );
+  }
 }
+export default connect(
+  null,
+  { setUser }
+)(withRouter(Login));
