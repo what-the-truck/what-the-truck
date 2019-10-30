ed74c6c88bbea5079dd71885b3e9c8dd34015c93
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-10-30 11:55:13 -0600

    delete/get event/attend

--- db/add_event.sql
+++ db/add_event.sql
@@ -1,2 +1,3 @@
--- INSERT into events (name,address,latitude,longitude,date,time)
--- VALUES($1,$2,$3,$4,$5,$6)
+INSERT into events (name,address,latitude,longitude,date,time)
+VALUES($1,$2,$3,$4,$5,$6)
+returning *;
--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -8,12 +8,29 @@ module.exports = {
     const db = req.app.get("db");
     let remove = await db.delete_event(id);
     res.status(200).send(remove);
+  },
+  addEvent: async (req, res) => {
+    const db = req.app.get("db");
+    const { name, address, latitude, longitude, date, time } = req.body;
+    const event = await db.add_event([
+      name,
+      address,
+      latitude,
+      longitude,
+      date,
+      time
+    ]);
+    res.status(200).send(event);
+  },
+  getAllAttend: async (req,res)=> {
+      const db =req.app.get('db')
+      const attend = await db.get_all_attend()
+      res.status(200).send(attend)
+  },
+  deleteAttend: async (req,res) =>{
+      const { id } = req.params
+      const db = req.app.get("db")
+      let remove = await db.delete_attend(id)
+      res.status(200).send(remove)
   }
-  //   addEvent: async (req,res) => {
-
-  //       const {} = req.body
-  //       const db = req.app.get('db')
-  //       let event = await db.add_event([])
-  //       res.status(200).send(event)
-  //   }
 };
