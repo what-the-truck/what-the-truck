7748be79b312f34e5c1970904a7f9d3104d8c2c6
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-10-31 23:36:16 -0600

    truckinfo

--- src/Twilio.js
+++ src/Twilio.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-require('dotenv/config')
+// import React, { Component } from 'react'
+// import axios from 'axios'
+// require('dotenv/config')
 
 
-export default class Twilio extends Component {
-    state = {
-        text:{
-            recipient:'' ,//needs a number to alert
-            textmessage:'' //figure out what our message will entail
-        }
-    }
+// export default class Twilio extends Component {
+//     state = {
+//         text:{
+//             recipient:'' ,//needs a number to alert
+//             textmessage:'' //figure out what our message will entail
+//         }
+//     }
 
-    sendText = () => {
-        const {text} = this.state
-        //pass text variables via query
-        axios.get(`/send-text?recipient=${text.recipient}&textmessage=${text.message}`)
-    }
+//     sendText = () => {
+//         const {text} = this.state
+//         //pass text variables via query
+//         axios.get(`/send-text?recipient=${text.recipient}&textmessage=${text.message}`)
+//     }
 
 
 
-    render() {
-        return (
-            <div>
-              <h1>Twilio</h1>  
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//               <h1>Twilio</h1>  
+//             </div>
+//         )
+//     }
+// }
